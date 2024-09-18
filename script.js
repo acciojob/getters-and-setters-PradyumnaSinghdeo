dcaa6e5cdc8546ebb64d20b72e619bd77bb26221
Pradyumna Singh Deo <87163520+PradyumnaSinghdeo@users.noreply.github.com>
2024-09-18 12:57:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-//complete this code
-class Person {}
 
-class Student extends Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Teacher extends Person {}
+  
+  get name() {
+    return this._name;
+  }
+
+  // Setter for age
+  set age(newAge) {
+    this._age = newAge;
+  }
+
+  
+  get age() {
+    return this._age;
+  }
+}
+
+
+class Student extends Person {
+  // Method for studying
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+
+class Teacher extends Person {
+  
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
